0dcc8dc3ed729d6511229407e593ca617300436f
LEVIS RABAH <levisrabah@gmail.com>
2024-03-19 13:14:30 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return cityName === 'NYC'? 'Ok, sounds good.':'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipType){
+  if (tipType ==='generous'){
+    return 'Thank you so much.';
+  }else if (tipType ==='not as generous'){
+    return 'Thank you.'
+  }return 'Bye.'
+  
 }
